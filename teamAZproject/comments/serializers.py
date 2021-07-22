27809f2ae8a642bb2bda0b6b0a0e4a456beecad6
Zachary <taylorzw96@gmail.com>
2021-07-22 10:11:52 -0500

    Added to the serializer to combine, comment list view class updated

--- teamAZproject/comments/serializers.py
+++ teamAZproject/comments/serializers.py
@@ -6,3 +6,9 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ['video_Id', 'comment_to_video', 'like_video', 'dislike_video']
+
+
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reply
+        fields = ['reply_to_comment']
--- teamAZproject/comments/views.py
+++ teamAZproject/comments/views.py
@@ -8,12 +8,17 @@ from rest_framework import status
 
 # Create your views here.
 class CommentList(APIView):
-
     def get(self, request):
-        pass
+        comment = Comment.object.all()
+        serializer = CommentSerializer(comment, many=True)
+        return Response(serializer.data)
 
     def post(self,request):
-        pass
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CommentDetail(APIView):
@@ -21,11 +26,23 @@ class CommentDetail(APIView):
     def get_object(self, pk):
         pass
 
-    def get(self, request, pk):
-        pass
-
     def put(self, request, pk):
         pass
 
-    def delete(self, request, pk):
+# Create your views here.
+class ReplyList(APIView):
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class ReplyDetail(APIView):
+
+    def get_object(self, pk):
+        pass
+
+    def put(self, request, pk):
         pass
